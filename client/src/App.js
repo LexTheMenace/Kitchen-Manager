fce27c8230a706f736e6424bc0b2e2ad15256c64
LexTheMenace <lexthemenace@gmail.com>
2020-10-20 11:37:42 -0400

    fetching from api and display

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { RecipeApp } from './components/recipe/RecipeApp';
 
 function App() {
   return (
     <div className="App">
       <h1>App.js</h1>
+      <RecipeApp/>
     </div>
   );
 }
---
+++ client/src/components/recipe/RecipeApp.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { fetchMeals } from './api/fetchMeals';
+// Change to get all by id's and make searchable? 
+export const RecipeApp = () => {
+    const [ query, setQuery ] = useState('');
+    const [ recipes, setRecipes ] = useState([]);
+
+    const search = async (e) => {
+        if(e.key === 'Enter'){
+            const data = await fetchMeals(query);
+           setRecipes(data.meals);
+           console.log(data.meals[0]);
+           setQuery('');
+        }
+    }
+    return (
+        <div className='main-container'>
+            <input
+                type='text'
+                className='search'
+                placeholder='Search by Category...'
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+            />      
+
+            {recipes.length > 0 && (
+                <div>
+                {recipes.map( recipe => {
+                    return (
+                        <div>
+                        <img src={recipe.strMealThumb}/>
+                        <h1 key={recipe.idMeal}>{recipe.strMeal}</h1>
+                        </div>
+                    )
+                })}
+                </div>
+            )}  
+        </div>
+    )
+}
